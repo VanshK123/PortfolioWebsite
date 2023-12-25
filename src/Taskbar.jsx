3ab1dc5159839ffd9ca8eda95c5590d751bb293c
Vansh Khanna <khanna57@purdue.edu>
2023-12-25 16:30:26 -0500

    added icons to the taskbar

--- src/Taskbar.jsx
+++ src/Taskbar.jsx
@@ -9,13 +9,13 @@ import pyLogo from './images/python-5.svg';
 import cLogo from './images/c.svg';
 
 const tabsData = [
-  { id: 1, title: 'home.jsx' },
-  { id: 2, title: 'about.html' },
-  { id: 3, title: 'contact.css' },
-  { id: 4, title: 'projects.cpp'},
-  { id: 5, title: 'compiler.java'},
-  { id: 6, title: 'github.py'},
-  { id: 7, title: 'resume.c'},
+  { id: 1, title: 'home.jsx', icon: reactLogo },
+  { id: 2, title: 'about.html', icon: htmlLogo },
+  { id: 3, title: 'contact.css', icon: cssLogo },
+  { id: 4, title: 'projects.cpp', icon: cppLogo },
+  { id: 5, title: 'compiler.java', icon: javaLogo },
+  { id: 6, title: 'github.py', icon: pyLogo },
+  { id: 7, title: 'resume.c', icon: cLogo },
 ];
 
 const Taskbar = () => {
@@ -33,6 +33,7 @@ const Taskbar = () => {
           className={`taskbar-tab ${tab.id === activeTab ? 'active' : ''}`}
           onClick={() => handleTabClick(tab.id)}
         >
+          <img src={tab.icon} alt={tab.title} className="tab-icon" />
           {tab.title}
         </div>
       ))}
