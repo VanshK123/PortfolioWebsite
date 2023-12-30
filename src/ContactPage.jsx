c0904390fda8cfa0802fff22fdc88cd31c6d8baf
Vansh Khanna <khanna57@purdue.edu>
2023-12-30 12:11:52 -0500

    added contact page

--- src/ContactPage.jsx
+++ src/ContactPage.jsx
@@ -1,29 +1,19 @@
 import React from 'react';
-import './ContactPage.css'; // Import your CSS file for styling
+import './ContactPage.css';
 
 const ContactPage = () => {
   return (
     <div className="vscode-container">
-      <div className="vscode-header">
-        <div className="vscode-titlebar">
-          <div className="vscode-title">Contact Page</div>
-        </div>
-      </div>
-      <div className="vscode-content">
-        <div className="vscode-editor">
-          <div className="editor-content">
-            <div className="contact-details">
-              <div className="contact-detail">
-                <div className="detail-label">Email:</div>
-                <div className="detail-value">khanna57@purdue.edu</div>
-              </div>
-              <div className="contact-detail">
-                <div className="detail-label">Phone:</div>
-                <div className="detail-value">+1 (765) 701 9288</div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <h1>Reach Out</h1>
+      <div className='content'>
+        <div style={{ color: '#007acc' }}>.socails <span style={{ color: 'white' }}>{"{"}</span></div>
+        
+          <div style={{ color: 'white' , marginLeft:50}}>email{":"} <a href='mailto:khanna57@purdue.edu' style={{ color: '#007acc' }} target='_blank' rel='noopener' className='links'>khanna57@purdue.edu</a>{";"}</div>
+          <div style={{ color: 'white', marginLeft:50 }}>phone{":"} <span style={{ color: '#007acc' }}>+1 765-701-9288</span>{";"}</div>
+          <div style={{ color: 'white', marginLeft:50 }}>linkedin{":"} <a href='https://www.linkedin.com/in/vansh-khanna-7026741b3/' style={{ color: '#007acc' }} target='_blank' rel='noopener'className='links'>vanshkhanna</a>{";"}</div>
+          <div style={{ color: 'white' , marginLeft:50}}>github{":"} <a href='https://github.com/VanshK123' style={{ color: '#007acc' }} target='_blank' rel='noopener' className='links'>VanshK123</a>{";"}</div>
+      
+        <div>{"}"}</div>
       </div>
     </div>
   );
