a2e8e24f20562756ac627d0124fa5eceb15dfdcc
Vansh Khanna <khanna57@purdue.edu>
2024-01-01 11:25:14 -0500

    changed layout of the compiler page

--- src/CompilerPage.jsx
+++ src/CompilerPage.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { darcula } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import './CompilerPage.css';
 
 const cCode = `
 int main() {
@@ -21,22 +22,23 @@ int main() {
 
 const CompilerPage = () => {
   return (
-    <div>
-      <h1>Compiler</h1>
-      <div>Enter C code in the input field and see the generated assembly from my compiler in the output field</div>
-      <div>Click on the "Run" button to run the generated assembly in the simulator</div>
-      <pre>
-      <SyntaxHighlighter language="c" style={darcula}>
-      {cCode}
-    </SyntaxHighlighter>
-
-    </pre>
-
-      <iframe
-        title="Compiler Page"
-        src="/Compiler/Reference Implementation Web Portal · ECE 468_573_595.html"
-        style={{ width: '100%', height: '88vh', border: 'none' }}
-      />
+    <div className='container'>
+      <div className='contents'>
+        <h1>Compiler</h1>
+        <div>Enter C code in the input field and see the generated assembly from my compiler in the output field</div>
+        <pre>
+          <SyntaxHighlighter language="c" style={darcula}>
+            {cCode}
+          </SyntaxHighlighter>
+        </pre>
+      </div>
+      <div className='compiler'>
+        <iframe
+          title="Compiler Page"
+          src="/Compiler/Reference Implementation Web Portal · ECE 468_573_595.html"
+          style={{ width: '100%', height: '88vh', border: 'none' }}
+        />
+      </div>
     </div>
   );
 };
