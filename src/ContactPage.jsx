070223f90c280b7c97b1476631cfe61a83b8b6b5
Vansh Khanna <khanna57@purdue.edu>
2024-01-01 13:25:05 -0500

    fixed spelling in the contact page

--- src/ContactPage.jsx
+++ src/ContactPage.jsx
@@ -6,7 +6,7 @@ const ContactPage = () => {
     <div className="vscode-container">
       <h1>Reach Out</h1>
       <div className='content'>
-        <div style={{ color: '#007acc' }}>.socails <span style={{ color: 'white' }}>{"{"}</span></div>
+        <div style={{ color: '#007acc' }}>.socials <span style={{ color: 'white' }}>{"{"}</span></div>
         
           <div style={{ color: 'white' , marginLeft:50}}>email{":"} <a href='mailto:khanna57@purdue.edu' style={{ color: '#007acc' }} target='_blank' rel='noopener' className='links'>khanna57@purdue.edu</a>{";"}</div>
           <div style={{ color: 'white', marginLeft:50 }}>phone{":"} <span style={{ color: '#007acc' }}>+1 765-701-9288</span>{";"}</div>
