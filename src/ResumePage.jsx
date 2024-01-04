1d43d364308e71b4d4ece57d342315e2ab6e1b6e
Vansh Khanna <khanna57@purdue.edu>
2024-01-04 15:10:27 -0500

    fixed pdf viewing issues

--- src/ResumePage.jsx
+++ src/ResumePage.jsx
@@ -4,18 +4,19 @@ import Resume from "./Resume.pdf";
 function ResumePage() {
   return (
     <div style={{ minHeight: "100%", overflow: "hidden" }}>
-      <object
+      <iframe
+        title="Resume"
         width="100%"
         height="100%"
-        data={Resume}
+        src={Resume}
         type="application/pdf"
       >
         {/* Fallback content or message if PDF cannot be displayed */}
-        <p style={{ color: "black" }}>
+        <p>
           It appears your browser doesn't support embedded PDFs. You can
-          download the PDF <a href='https://www.linkedin.com/in/vansh-khanna-7026741b3/'>here</a>.
+          download the PDF <a href='https://drive.google.com/file/d/1SlUhxmfhruoV3m_er-yaJHf8GNc6foZh/view'>here</a>.
         </p>
-      </object>
+      </iframe>
     </div>
   );
 }
